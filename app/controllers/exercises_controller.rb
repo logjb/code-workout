076ec6b3b6bcf7ffadb98259d79916db21e2f648
logjb <bull.logan@yahoo.com>
2018-03-11 17:52:05 -0400

    Update exercises_controller.rb

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -3,7 +3,7 @@ class ExercisesController < ApplicationController
   require 'oauth/request_proxy/rack_request'
 
   load_and_authorize_resource
-  skip_authorize_resource only: :practice
+  skip_authorize_resource only: [:practice, :upload_create]
 
   #~ Action methods ...........................................................
   after_action :allow_iframe, only: [:practice, :embed]
