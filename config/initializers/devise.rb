ca1a1c5061376ad70f5e0f4bfa7b87f20f5acd62
logjb <bull.logan@yahoo.com>
2018-03-25 22:18:20 -0400

    Update devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -11,7 +11,8 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.mailer_sender =
-    'ayaan@vt.edu'
+    'logan.bull.13@cnu.edu'
+    #'ayaan@vt.edu'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
